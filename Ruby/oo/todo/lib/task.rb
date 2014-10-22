33cc7022ebf622f24d2b376b19cb74f8d0c568f8
emersonp <parker.emerson@gmail.com>
2014-10-22 05:24:11 -0700

    Added Task Completion to ToDo
    Added brute force item selection and completion to task class, and to to_do interface.

--- Ruby/oo/todo/lib/task.rb
+++ Ruby/oo/todo/lib/task.rb
@@ -1,9 +1,22 @@
 class Task
     def initialize(description)
         @description = description
+        @complete = false
     end
 
     def description
         @description
+    end
+
+    def complete?
+        @complete
+    end
+
+    def complete_toggle
+        if @complete
+            @complete = false
+        else
+            @complete = true
+        end
     end
 end
--- Ruby/oo/todo/to_do.rb
+++ Ruby/oo/todo/to_do.rb
@@ -6,6 +6,7 @@ def main_menu
     loop do
         puts "Press 'a' to add a task or 'l' to list all of your tasks."
         puts "Press 'x' to exit."
+        puts "Press 'c' to mark a task complete."
         main_choice = gets.chomp
         if main_choice == 'a'
             add_task
@@ -14,6 +15,8 @@ def main_menu
         elsif main_choice == 'x'
             puts "Good-bye!"
             exit
+        elsif main_choice == 'c'
+            mark_complete
         else
             puts "\nSorry, that wasn't a valid option.\n"
         end
@@ -29,10 +32,21 @@ end
 
 def list_tasks
     puts "\nHere are all of your tasks:"
+    menu_counter = 1
     @list.each do |task|
-        puts task.description
+        if not task.complete?
+            puts "#{menu_counter}: #{task.description}"
+            menu_counter += 1
+        end
     end
     puts "\n"
+end
+
+def mark_complete
+    puts "\nWhich task would you like to complete?"
+    list_tasks
+    menu_selection = gets.chomp
+    @list[menu_selection.to_i - 1].complete_toggle
 end
 
 main_menu
