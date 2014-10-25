2da9f37b126cb1c0683f003bb54b1da84f5075df
emersonp <parker.emerson@gmail.com>
2014-10-25 16:19:47 -0700

    First Bullet Point - Working Contacts
    Got contact classes and methods set up, pursuant to first bullet point of requirements.

--- ruby/oo/addbook/lib/contact.rb
+++ ruby/oo/addbook/lib/contact.rb
@@ -15,4 +15,63 @@ class Contact
     def full
         "#{@first} #{@last}"
     end
+
+    def set_phone(num)
+        @phone = num
+    end
+
+    def phone
+        @phone
+    end
+
+    def set_email(email)
+        @email = email
+    end
+
+    def email
+        @email
+    end
+
+    def set_address(street, city, state, zip)
+        @street = street
+        @city = city
+        @state = state
+        @zip = zip
+    end
+
+    def address
+        "#{@street}\n#{@city}, #{@state}\t#{@zip}"
+    end
+
+    def set_street(street)
+        @street = street
+    end
+
+    def street 
+        @street
+    end
+
+    def set_city(city)
+        @city = city
+    end
+
+    def city
+        @city
+    end
+
+    def set_state(state)
+        @state = state
+    end
+
+    def state
+        @state
+    end
+
+    def set_zip(zip)
+        @zip = zip
+    end
+
+    def zip
+        @zip
+    end
 end
--- ruby/oo/addbook/spec/addbook_spec.rb
+++ ruby/oo/addbook/spec/addbook_spec.rb
@@ -23,5 +23,39 @@ describe Contact do
         test_person = Contact.new('Frank', 'Beans')
         test_person.full.should eq 'Frank Beans'
     end
+    it 'lets you read the phone number out' do
+        test_person = Contact.new('Frank', 'Beans')
+        test_person.set_phone('5035551234')
+        test_person.phone.should eq '5035551234'
+    end
+    it 'lets you read the email out' do
+        test_person = Contact.new('Frank', 'Beans')
+        test_person.set_email('frank.beans@gmail.com')
+        test_person.email.should eq 'frank.beans@gmail.com'
+    end
+    it 'lets you read the address out' do
+        test_person = Contact.new('Frank', 'Beans')
+        test_person.set_street('1234 Main St')
+        test_person.set_city('Portland')
+        test_person.set_state('OR')
+        test_person.set_zip('97111')
+        test_person.street.should eq '1234 Main St'
+        test_person.city.should eq 'Portland'
+        test_person.state.should eq 'OR'
+        test_person.zip.should eq '97111'
+    end
+    it 'lets you set the full address' do
+        test_person = Contact.new('Frank', 'Beans')
+        test_person.set_address('1234 Main St', 'Portland', 'OR', '97111')
+        test_person.street.should eq '1234 Main St'
+        test_person.city.should eq 'Portland'
+        test_person.state.should eq 'OR'
+        test_person.zip.should eq '97111'
+    end
+    it 'lets you read out the full address' do
+        test_person = Contact.new('Frank', 'Beans')
+        test_person.set_address('1234 Main St', 'Portland', 'OR', '97111')
+        test_person.address.should eq "1234 Main St\nPortland, OR\t97111"
+    end
 end
 
